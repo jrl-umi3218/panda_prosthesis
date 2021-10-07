2a1ce1c7741f41ae1e107fe516c1c8bb8bd3afa8
Arnaud TANGUY <arn.tanguy@gmail.com>
2021-10-07 15:03:29 +0200

    [convex] Fix convex scale

--- convex/generate.sh
+++ convex/generate.sh
@@ -4,7 +4,7 @@
 
 # Requires
 # - qconvex (sudo apt install qhull-bin)
-# - mesh_sampling (https://github.com/arntanguy/mesh_sampling)
+# - mesh_sampling (https://github.com/jrl-umi3218/mesh_sampling)
 
 readonly this_dir=`cd $(dirname $0); pwd`
 readonly meshes_dir=`cd $this_dir/../meshes; pwd`
@@ -13,7 +13,7 @@ for m in `find $meshes_dir -type f -name '*.stl'`
 do
   qc_out=/tmp/`basename $m ".stl"`.qc
   ch_out=$this_dir/`basename $m ".stl"`-ch.txt
-  mesh_sampling $m $qc_out --type xyz --samples 1000
+  mesh_sampling $m $qc_out --type xyz --samples 1000 --scale 0.001
   qconvex TI $qc_out TO $ch_out Qt o f
   rm -f $qc_out
 done
