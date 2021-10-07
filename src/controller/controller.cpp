22fd162a5bd653b744ead56bad77a13d34307f8d
Arnaud TANGUY <arn.tanguy@gmail.com>
2021-10-07 10:32:00 +0200

    [rsdf] Load additional rsdf surfaces for tibia/femur

--- src/controller/controller.cpp
+++ src/controller/controller.cpp
@@ -1,4 +1,5 @@
 #include "controller.h"
+#include "config.h"
 
 namespace panda_prosthetics
 {
@@ -6,6 +7,18 @@ namespace panda_prosthetics
 PandaProsthetics::PandaProsthetics(mc_rbdyn::RobotModulePtr rm, double dt, const mc_rtc::Configuration & config)
 : mc_control::fsm::Controller(rm, dt, config), config_(config)
 {
+  // TODO in mc_rtc: support loading rsdf from multiple directories defined in the robot module
+  auto load_rsdf = [this](const std::string & robot_name)
+  {
+    auto & r = robot(robot_name);
+    const auto rsdf_dir = std::string{panda_prosthesis::rsdf_DIR} + "/" + robot_name;
+    mc_rtc::log::info("[{}] loading additional surfaces for robot {} from {}", this->name_, robot_name, rsdf_dir);
+    r.loadRSDFFromDir(rsdf_dir);
+    gui()->data()("surfaces").add(r.name(), r.availableSurfaces());
+  };
+
+  load_rsdf("panda_femur");
+  load_rsdf("panda_tibia");
 }
 
 bool PandaProsthetics::run()
