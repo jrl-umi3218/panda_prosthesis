c6a970decf05e6e5ca9472999ccbd7829623ea6c
Arnaud TANGUY <arn.tanguy@gmail.com>
2023-03-09 14:32:36 +0100

    Add forces in the gui

--- src/controller/PandaBraceController.cpp
+++ src/controller/PandaBraceController.cpp
@@ -8,11 +8,26 @@ namespace panda_prosthetics
 PandaBraceController::PandaBraceController(mc_rbdyn::RobotModulePtr rm, double dt, const mc_rtc::Configuration & config)
 : mc_control::fsm::Controller(rm, dt, config), config_(config)
 {
-  gui()->addElement({"Frames"},
-                    mc_rtc::gui::Transform("Femur", [this]() { return robot().frame("Femur").position(); }));
-  gui()->addElement({"Frames"}, mc_rtc::gui::Transform("Tibia", [this]() {
-                      return robot("brace_bottom_setup").frame("Tibia").position();
-                    }));
+  auto & gui = *this->gui();
+  gui.addElement({"PandaBrace", "Frames"},
+                    mc_rtc::gui::Transform("Femur", [this]() { return robot().frame("Femur").position(); }),
+  		    mc_rtc::gui::Transform("Tibia", [this]() { return robot("brace_bottom_setup").frame("Tibia").position(); }));
+
+  gui.addElement({"PandaBrace", "Forces"},
+   mc_rtc::gui::Label("Details",
+     [this]() { return std::string{"Forces without gravity expressed in the reference frame"}; }));
+  gui.addElement({"PandaBrace", "Forces", robot().name()},
+                    mc_rtc::gui::Force("FemurWrench",
+                      [this]() { return robot().frame("Femur").wrench(); },
+                      [this]() { return robot().frame("Femur").position(); }));
+  gui.addElement({"PandaBrace", "Forces", "brace_bottom_setup"},
+                    mc_rtc::gui::Force("TibiaWrench",
+                      [this]() { return robot("brace_bottom_setup").frame("Tibia").wrench(); },
+                      [this]() { return robot("brace_bottom_setup").frame("Tibia").position(); }));
+  logger().addLogEntry("controlRobot_Frame_Femur",
+   [this]() { return robot().frame("Femur").position(); });
+  logger().addLogEntry("realRobot_Frame_Femur",
+   [this]() { return realRobot().frame("Femur").position(); });
 }
 
 bool PandaBraceController::run()
