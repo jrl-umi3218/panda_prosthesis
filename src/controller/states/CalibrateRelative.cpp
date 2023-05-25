8b30008f4fd959afbf9ff272665b1d198c60c3fc
Arnaud TANGUY <arn.tanguy@gmail.com>
2023-05-25 15:26:51 +0200

    Fix GoRelative

--- src/controller/states/CalibrateRelative.cpp
+++ src/controller/states/CalibrateRelative.cpp
@@ -26,37 +26,26 @@ void CalibrateRelative::start(mc_control::fsm::Controller & ctl)
   {
     mc_rtc::log::error_and_throw("[{}] No frame named {} in robot {}", name(), targetFrameName, targetRobotName);
   }
-
-  // 1- Suppose that the tibia is parallel to the floor
-  // 2- Suppose that the robot femur axis has been placed at a known position X_tibia_femur w.r.t the tibia axis (manually)
-  auto X_tibia_femur = config_("relativePose", sva::PTransformd::Identity()); // XXX accurate transform to be provided by Lea
-  // 3- Move tibia position to match this known position 
+  
+  // Suppose that the femur and calibration tool are in perfect contact
+  // and that the relative position between the femur and tibia frames is fully known in that configuration
+  auto X_tibia_femur = config_("relativePose", sva::PTransformd::Identity());
   auto X_0_femur = ctl.robot().frame(frameName).position();
   auto X_0_tibia = X_tibia_femur.inv() * X_0_femur;
-  auto X_tibia_base = ctl.robot(targetRobotName).posW() * ctl.robot(targetRobotName).frame(targetFrameName).position().inv();
-  Eigen::Matrix3d mergeRPY = stateObservation::kine::mergeRoll1Pitch1WithYaw2AxisAgnostic(Eigen::Matrix3d::Identity(), X_0_tibia.rotation()); 
-  X_0_tibia.rotation() = mergeRPY; 
-  auto X_0_tibiaBase = X_tibia_base * X_0_tibia;
-  ctl.robot(targetRobotName).posW(X_0_tibiaBase);
-
-  // 4- Add task on femur to move it to a known transformation between tibia and femur 
-  auto X_0_femurTarget = X_tibia_femur * X_0_tibia;
-  transformTask_ = std::make_shared<mc_tasks::BSplineTrajectoryTask>(ctl.robot(robotName).frame(frameName), 5.0, 1000, 1000, X_0_femurTarget);
-  transformTask_->reset();
-  ctl.solver().addTask(transformTask_);
+  auto X_tibia_base =  ctl.robot(targetRobotName).posW() * ctl.robot(targetRobotName).frame(targetFrameName).position().inv();
+  auto X_0_base = X_tibia_base * X_0_tibia;
+  ctl.robot(targetRobotName).posW(X_0_base);
 
   output("OK");
 }
 
 bool CalibrateRelative::run(mc_control::fsm::Controller & ctl)
 {
-  return transformTask_->timeElapsed();
+  return true;
 }
 
 void CalibrateRelative::teardown(mc_control::fsm::Controller & ctl)
 {
-
-  ctl.solver().removeTask(transformTask_);
 }
 
 EXPORT_SINGLE_STATE("CalibrateRelative", CalibrateRelative)
--- src/controller/states/CalibrateRelative.h
+++ src/controller/states/CalibrateRelative.h
@@ -6,10 +6,6 @@
 struct CalibrateRelative : mc_control::fsm::State
 {
   void start(mc_control::fsm::Controller & ctl) override;
-
   bool run(mc_control::fsm::Controller & ctl) override;
-
   void teardown(mc_control::fsm::Controller & ctl) override;
-protected:
-  std::shared_ptr<mc_tasks::BSplineTrajectoryTask> transformTask_;
 };
