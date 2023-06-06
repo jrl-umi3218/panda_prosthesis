72ae4a0448b4b2bdd4bb8cc517d2e30f441e9b7a
Arnaud TANGUY <arn.tanguy@gmail.com>
2023-06-06 15:45:25 +0200

    Fix calibration with new model and anatomical frames

--- src/controller/states/CalibrateRelative.cpp
+++ src/controller/states/CalibrateRelative.cpp
@@ -4,7 +4,6 @@
 
 void CalibrateRelative::start(mc_control::fsm::Controller & ctl)
 {
-  
   auto robotName = static_cast<std::string>(config_("robot"));
   auto frameName = static_cast<std::string>(config_("frame"));
   if(!ctl.hasRobot(robotName))
@@ -26,14 +25,20 @@ void CalibrateRelative::start(mc_control::fsm::Controller & ctl)
   {
     mc_rtc::log::error_and_throw("[{}] No frame named {} in robot {}", name(), targetFrameName, targetRobotName);
   }
-  
+
   // Suppose that the femur and calibration tool are in perfect contact
   // and that the relative position between the femur and tibia frames is fully known in that configuration
-  auto X_tibia_femur = config_("relativePose", sva::PTransformd::Identity());
-  auto X_0_femur = ctl.robot().frame(frameName).position();
-  auto X_0_tibia = X_tibia_femur.inv() * X_0_femur;
-  auto X_tibia_base =  ctl.robot(targetRobotName).posW() * ctl.robot(targetRobotName).frame(targetFrameName).position().inv();
-  auto X_0_base = X_tibia_base * X_0_tibia;
+  auto X_femur_tibia = config_("femurToTibia", sva::PTransformd::Identity());
+  auto X_0_femur = ctl.robot(robotName).frame(frameName).position();
+  auto X_0_tibia = X_femur_tibia * X_0_femur;
+
+  // Get relative transformation between tibia frame and brace_bottom_setup base link
+  auto X_0_tibiaF = ctl.robot(targetRobotName).frame(targetFrameName).position();
+  auto X_0_baseF = ctl.robot(targetRobotName).posW();
+  auto X_base_tibia = X_0_tibiaF * X_0_baseF.inv();
+
+  auto X_0_base = X_base_tibia.inv() * X_0_tibia;
+  mc_rtc::log::critical("X_0_base: {}", X_0_base.translation().transpose());
   ctl.robot(targetRobotName).posW(X_0_base);
 
   output("OK");
