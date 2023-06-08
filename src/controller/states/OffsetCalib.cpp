c88778db0ac440f5c37f5472086a25c23e82e966
Arnaud TANGUY <arn.tanguy@gmail.com>
2023-06-08 16:33:11 +0200

    Add offset to compensate wrong calib tool

--- src/controller/states/OffsetCalib.cpp
+++ src/controller/states/OffsetCalib.cpp
@@ -3,75 +3,50 @@
 
 void OffsetCalib::start(mc_control::fsm::Controller & ctl)
 {
+  auto robotName = static_cast<std::string>(config_("robot"));
+  auto frameName = static_cast<std::string>(config_("frame"));
+  if(!ctl.hasRobot(robotName))
+  {
+    mc_rtc::log::error_and_throw("[{}] No robot named {}", name(), robotName);
+  }
+  if(!ctl.robot(robotName).hasFrame(frameName))
+  {
+    mc_rtc::log::error_and_throw("[{}] No frame named {} in robot {}", name(), frameName, robotName);
+  }
 
-  ctl.gui()->addElement(
-      this, {"offset"},
-      mc_rtc::gui::Button(
-          "offset_calib",
-          [this, &ctl]()
-          {
-            auto robotName = static_cast<std::string>(config_("robot"));
-            auto frameName = static_cast<std::string>(config_("frame"));
-            if(!ctl.hasRobot(robotName))
-            {
-              mc_rtc::log::error_and_throw("[{}] No robot named {}", name(), robotName);
-            }
-            if(!ctl.robot(robotName).hasFrame(frameName))
-            {
-              mc_rtc::log::error_and_throw("[{}] No frame named {} in robot {}", name(), frameName, robotName);
-            }
+  auto refRobotName = static_cast<std::string>(config_("refRobot"));
+  auto refFrameName = static_cast<std::string>(config_("refFrame"));
 
-            auto targetRobotName = static_cast<std::string>(config_("target_robot"));
-            auto targetFrameName = static_cast<std::string>(config_("target_frame"));
+  if(!ctl.hasRobot(refRobotName))
+  {
+    mc_rtc::log::error_and_throw("[{}] No robot named {}", name(), refRobotName);
+  }
+  if(!ctl.robot(refRobotName).hasFrame(refFrameName))
+  {
+    mc_rtc::log::error_and_throw("[{}] No frame named {} in robot {}", name(), refFrameName, refRobotName);
+  }
+  offsetTask_ = std::make_shared<mc_tasks::TransformTask>(ctl.robot(robotName).frame(frameName), 20.0, 1000.0);
+  offsetTask_->reset();
+  auto X_0_refFrame = ctl.robot(refRobotName).frame(refFrameName).position();
+  auto X_0_targetFrame = ctl.robot(robotName).frame(frameName).position();
+  auto X_refFrame_targetFrame = X_0_targetFrame * X_0_refFrame.inv(); // current pose of target wrt to ref frame
+  auto offset = config_("offset", sva::PTransformd::Identity());
+  auto X_0_target = X_refFrame_targetFrame * offset * X_0_refFrame;
+  offsetTask_->target(X_0_target);
 
-            if(!ctl.hasRobot(targetRobotName))
-            {
-              mc_rtc::log::error_and_throw("[{}] No robot named {}", name(), targetRobotName);
-            }
-            if(!ctl.robot(targetRobotName).hasFrame(targetFrameName))
-            {
-              mc_rtc::log::error_and_throw("[{}] No frame named {} in robot {}", name(), targetFrameName,
-                                           targetRobotName);
-            }
-            offsetTask_ = std::make_shared<mc_tasks::TransformTask>(ctl.robot(robotName).frame(frameName), 2.0, 500.0);
-            auto X_tibia_femur = sva::PTransformd(Eigen::Vector3d{0.01, 0, 0});
-            auto X_0_femur = X_tibia_femur * ctl.robot(robotName).frame(frameName).position();
-            offsetTask_->target(X_0_femur);
-
-            okep = true;
-
-            ctl.solver().addTask(offsetTask_);
-            mc_rtc::log::info("Offset calib Ok");
-            output("OK");
-          }),
-      mc_rtc::gui::Button("Pass", [this, &ctl]() { go_next = true; }));
+  crit.configure(*offsetTask_, ctl.timeStep, config_("completion"));
+  ctl.solver().addTask(offsetTask_);
+  output("OK");
 }
 
 bool OffsetCalib::run(mc_control::fsm::Controller & ctl)
 {
-
-  if(okep)
-  {
-    if(!go_next)
-    {
-    }
-    if(go_next)
-    {
-      return true;
-    }
-  }
-  return false;
+  return crit.completed(*offsetTask_);
 }
 
 void OffsetCalib::teardown(mc_control::fsm::Controller & ctl)
 {
   ctl.solver().removeTask(offsetTask_);
-  ctl.getPostureTask(config_("robot"))->reset();
-  // XXX set high stiffness so that the robot stays here while selecting the trajectory
-  // consider a better way
-  ctl.getPostureTask(config_("robot"))->stiffness(100);
-  ctl.getPostureTask(config_("robot"))->weight(10);
-  // ctl.solver().removeTask(targetTask_);
   ctl.gui()->removeElements(this);
 }
 
--- src/controller/states/OffsetCalib.h
+++ src/controller/states/OffsetCalib.h
@@ -1,21 +1,16 @@
 #pragma once
 
+#include <mc_control/CompletionCriteria.h>
 #include <mc_control/fsm/State.h>
 #include <mc_tasks/TransformTask.h>
 
 struct OffsetCalib : mc_control::fsm::State
 {
   void start(mc_control::fsm::Controller & ctl) override;
-
   bool run(mc_control::fsm::Controller & ctl) override;
-
   void teardown(mc_control::fsm::Controller & ctl) override;
 
-  void load(mc_control::fsm::Controller & ctl);
-
 private:
-  bool okep = false;
-  bool go_next = false;
+  mc_control::CompletionCriteria crit;
   std::shared_ptr<mc_tasks::TransformTask> offsetTask_;
-  double err = 100;
 };
